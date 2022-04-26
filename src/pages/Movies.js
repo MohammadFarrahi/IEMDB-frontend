bf214ab7b8a459a3a512c2ab57cc921f463f97ab
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2022-04-26 23:16:32 +0430

    Complete Movies page

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -8,12 +8,17 @@ export default function Movies() {
   const [movies, setMovies] = useState();
 
   useEffect(() => {
-    const response = axios.get('movies/');
-    console.log(response)
-    const movieList = response['data'];
-    setMovies(movieList);
+    async function fetchData() {
+      try {
+        const response = await axios.get('movies');
+        const movieList = response.data;
+        setMovies(movieList);
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fetchData();
   }, [])
-
   return (
     <div className="container-fluid p-0">
       <div className="row m-0 movies-container">
@@ -21,15 +26,27 @@ export default function Movies() {
         </div>
         <div className="col-8 movies-body">
           <div className="movie-picture-list container-fluid">
-            {movies && 
-              movies.map(item => (
-                <div className="col-3">
-                  <MoviePreview image={item.coverImgUrl} name={item.rate} rate={item.imdbRate} />
-                </div>
-              ))
-            }
+            <div className="row p-3">
+              {movies &&
+                movies.map(item => (
+                  <div className="col-3 mb-3" key={item.id}>
+                    <MoviePreview image={item.coverImgUrl} name={item.name} rate={item.imdbRate} />
+                  </div>
+                ))
+              }
+            </div>
           </div>
         </div>
+        <div class="col-2">
+        <div class="rank-box-container">
+          <span dir="rtl" class="lead">رتبه‌بندی بر اساس:</span>
+          <div class="rank-box">
+            <span dir="rtl" class="lead"> تاریخ</span>
+            <span dir="rtl" class="lead">امتیاز imdb</span>
+          </div>
+        </div>
+      </div>
+
       </div>
     </div>
   )
