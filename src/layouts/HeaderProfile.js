f6908e210d0775f5a1a51a8028d0677c24fa93d8
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2022-04-27 11:42:38 +0430

    Encapsulate header profile in a new component

---
+++ src/layouts/HeaderProfile.js
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import './HeaderProfile.css';
+import './Dropdown.css';
+
+
+export default function HeaderProfile(props) {
+  var isLoggedIn = localStorage.getItem('userLoggedIn');
+  var userId = localStorage.getItem('userId');
+
+  return (
+    <div className="dropdown porfile-dropdown-contianer">
+      <span
+        id="profileDropdown"
+        data-toggle="dropdown"
+        className="profile-icon iconify-inline"
+        data-icon="healthicons:ui-user-profile-negative"
+      ></span>
+      <div className="profile-menu dropdown-menu " aria-labelledby="profileDropdown">
+        {isLoggedIn ?
+          <>
+            <span className="menu-item dropdown-item ">{userId}</span>
+            <Link className="menu-item dropdown-item " to='/watchlist'>watch list</Link>
+          </>
+          :
+          <>
+            <Link className="menu-item dropdown-item " to="/login">ورود</Link>
+            <Link className="menu-item dropdown-item " to='signup'>ثبت نام</Link>
+          </>
+        }
+      </div>
+    </div>
+  )
+}
