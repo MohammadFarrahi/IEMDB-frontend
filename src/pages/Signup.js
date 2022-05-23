cbe27c76425cd685bec4de65e629fda10f8a24fa
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2022-05-23 22:39:03 +0430

    Add exception handling for Signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,20 +1,23 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import logo from '../images/logo.png'
 import './Login.css';
 
 
 export default function Signup() {
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [nickname, setNickname] = useState('');
   const [name, setName] = useState('');
   const [birthDate, setBirthDate] = useState('');
 
-
+  const [error, setError] = useState('');
 
   const handleSignup = async (e) => {
     e.preventDefault();
+    setError('');
     try {
       const data = { email, password, nickname, birthDate, name};
       const response = await Request.post('/auth/signup/', data, {});
@@ -24,7 +27,7 @@ export default function Signup() {
       }
 
     } catch (e) {
-      setError('ایمیل یا رمز عبور صحیح نیست')
+      setError('عملیات با موفقیت انجام نشد')
     }
 
   }
@@ -100,9 +103,14 @@ export default function Signup() {
             />
             <label className="form-label" for="form2Example11">تاریخ تولد</label>
           </div>
+          {error &&
+              <div className="text-center pt-1 mb-5 pb-1">
+                <span className='text-danger'>{error}</span>
+              </div>
+            }
 
           <div className="text-center pt-1 mb-5 pb-1">
-            <button className="btn btn-danger btn-block fa-lg gradient-custom-2 mb-3" type="button">ثبت نام</button>
+            <button className="btn btn-danger btn-block fa-lg gradient-custom-2 mb-3" type="submit">ثبت نام</button>
           </div>
 
           <div className="d-flex align-items-center justify-content-center pb-4">
