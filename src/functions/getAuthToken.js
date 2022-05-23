99a0c6d91354dcb457c0ba49a03014f47d855a4c
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2022-05-23 22:22:02 +0430

    Remove 'Token ' from the Authorization value in header

--- src/functions/getAuthToken.js
+++ src/functions/getAuthToken.js
@@ -1,9 +1,9 @@
 export function getAuthToken(token = null) {
   if (token === null) {
     const savedToken = localStorage.getItem('token');
-    return { 'Authorization': 'Token ' + savedToken, }
+    return { 'Authorization': savedToken, }
   }
   else {
-    return { 'Authorization': 'Token ' + token, }
+    return { 'Authorization':  token, }
   }
 }
