a4c766e1438c7905cb52fa555e57704216d2a465
Mohammad Farrahi <farahim.1379@gmail.com>
2022-06-19 02:30:43 +0430

    Add ENV variable

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./app/App";
 
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
+axios.defaults.baseURL = process.env.API_URL;
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
